61ef14745e44c94398383b615d6552d85357a907
“ponkys” <diego.mosquera.soto@gmail.comssh-keygen -t rsa -C diego.mosquera.soto@gmail.comgit config --global color.ui truegit config --global user.name ponkysgit config --global user.email diego.mosquera.soto@gmail.com>
2016-01-19 18:51:24 +0000

    no funciona amazon s3 validacion modelo episodio para mp3

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -4,4 +4,7 @@ class Episode < ActiveRecord::Base
   has_attached_file :episode_thumbnail, styles: { large: "700x700#", medium: "500x500#" }
   validates_attachment_content_type :episode_thumbnail, content_type: /\Aimage\/.*\Z/
 
+  has_attached_file :mp3, styles: { large: "700x700#", medium: "500x500#" }
+  validates_attachment_content_type :mp3, :content_type => { :content_type => ["audio/mpeg","audio/mp3"] }, :file_name => { :matches => [/mp3\z/] }
+
 end
