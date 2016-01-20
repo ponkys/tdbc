0e755d87df99f73779ddcb55c65319f497b692fa
“ponkys” <diego.mosquera.soto@gmail.comssh-keygen -t rsa -C diego.mosquera.soto@gmail.comgit config --global color.ui truegit config --global user.name ponkysgit config --global user.email diego.mosquera.soto@gmail.com>
2016-01-20 01:49:59 +0000

    pagination

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'paperclip', '~> 4.3', '>= 4.3.2'
 
 gem 'aws-sdk', '< 2.0'
 
+gem 'will_paginate', '~> 3.1'
+
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -18,7 +18,7 @@ class EpisodesController < ApplicationController
 	end
 
 	def show
-		@episodes = Episode.where(podcast_id: @podcast).order("created_at DESC").reject { |e| e.id == @episode.id } #revisar porque se sale
+		@episodes = Episode.where(podcast_id: @podcast).order("created_at DESC").reject { |e| e.id == @episode.id }
 	end
 
 	def edit
