50f5287cbf433fe4241849feba5b815783375f5c
“ponkys” <diego.mosquera.soto@gmail.comssh-keygen -t rsa -C diego.mosquera.soto@gmail.comgit config --global color.ui truegit config --global user.name ponkysgit config --global user.email diego.mosquera.soto@gmail.com>
2016-02-24 18:38:48 +0000

    Creacion de controlador devise Admin con su modelo y relaciones con podcasts

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,12 +1,8 @@
-class Podcast < ActiveRecord::Base
+class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :episodes
-  belongs_to :admin
-
-  has_attached_file :thumbnail, styles: { large: "700x700#", medium: "500x500#" }
-  validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\Z/
+	has_many :podcasts
 end
