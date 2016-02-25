66cb6ffd6c7bf59e2c4e77e341f3db1f89023c18
“ponkys” <diego.mosquera.soto@gmail.comssh-keygen -t rsa -C diego.mosquera.soto@gmail.comgit config --global color.ui truegit config --global user.name ponkysgit config --global user.email diego.mosquera.soto@gmail.com>
2016-02-25 13:39:08 +0000

    empezar la migracion con el dashboard de admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,2 +1,22 @@
 class AdminsController < ApplicationController
+	before_action :authenticate_admin!
+
+	before_action :find_admin, only: [:dashboard]
+	before_action :find_podcasts, only: [:show, :dashboard]
+	before_action :find_episodes, only: [:show, :dashboard]
+
+	def dashboard
+		
+	end
+
+	private
+
+	def find_episodes
+		@episodes = Episode.all.order("created_at DESC")
+		
+	end
+
+	def find_podcasts
+		@podcast = Podcast.all.order("created_at DESC")
+	end
 end
