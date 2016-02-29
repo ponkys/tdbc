c48e73d1ec9182d490198d30b868f16437f119e6
“ponkys” <diego.mosquera.soto@gmail.comssh-keygen -t rsa -C diego.mosquera.soto@gmail.comgit config --global color.ui truegit config --global user.name ponkysgit config --global user.email diego.mosquera.soto@gmail.com>
2016-02-29 21:51:20 +0000

    mail features

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,18 @@ Rails.application.configure do
     }
   }
 
+  config.action_mailer.default_url_options = { host: 'https://thawing-tor-3074.herokuapp.com/'}
+
+  config.action_mailer.delivery_metho = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
 end
