4d49c30e07f3f42e08bc7a4725f55eadb5005976
“ponkys” <diego.mosquera.soto@gmail.comssh-keygen -t rsa -C diego.mosquera.soto@gmail.comgit config --global color.ui truegit config --global user.name ponkysgit config --global user.email diego.mosquera.soto@gmail.com>
2016-02-29 19:34:25 +0000

    production env for sendgrit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,5 +102,14 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+  address: "smtp.sendgrid.net",
+  port: 587,
+  domain: 'heroku.com',
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD']
+}
 
 end
