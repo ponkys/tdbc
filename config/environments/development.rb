e40074521ea6447f0636b64a608969d3eb276eeb
“ponkys” <diego.mosquera.soto@gmail.comssh-keygen -t rsa -C diego.mosquera.soto@gmail.comgit config --global color.ui truegit config --global user.name ponkysgit config --global user.email diego.mosquera.soto@gmail.com>
2016-03-01 15:35:05 +0000

    email working with email catcher in dv mode finally

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,20 +41,23 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { :host => 'http://localhost:3000/' }
-
   config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
+  # config.action_mailer.default_url_options = { :host => 'http://localhost:3000/' }
+
+  # config.action_mailer.delivery_method = :smtp
 
   Rails.application.routes.default_url_options[:host] = 'http://localhost:3000/'
 
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+  # ActionMailer::Base.smtp_settings = {
+  #   :address        => 'smtp.sendgrid.net',
+  #   :port           => '587',
+  #   :authentication => :plain,
+  #   :user_name      => ENV['SENDGRID_USERNAME'],
+  #   :password       => ENV['SENDGRID_PASSWORD'],
+  #   :domain         => 'heroku.com',
+  #   :enable_starttls_auto => true
+  # }
 
 end
